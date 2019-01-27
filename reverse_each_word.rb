0097130004db9911f0f1a3bce92fe68948d640b9
Allen F Partlow <allenfp@gmail.com>
2019-01-27 23:37:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,13 @@
+x = "Hello there my friend."
+
 def reverse_each_word(sentence1)
   sentence2 = ""
   array = sentence1.split(" ")
   array.collect do |y|
     puts y.reverse!
-    print y.reverse!
+
   end
 
 end
+
+reverse_each_word(x)
